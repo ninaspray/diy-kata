3b78bb2e40f19d3207c424186a979ca3c558fe2c
Nina Spray <ninaspray@gmail.com>
2021-01-06 08:31:11 +0000

    fizzBuzz complete - last one to question

--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,26 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  test("returns fizz when passed a multiple of 3", () => {
+    expect(fizzBuzz(3)).toBe("fizz");
+    expect(fizzBuzz(27)).toBe("fizz");
+  });
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+  test("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+  });
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  test("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("fizzBuzz");
+    expect(fizzBuzz(45)).toBe("fizzBuzz");
+  });
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  test("returns the number when it isn't a multiple of 3 or 5", () => {
+    expect(fizzBuzz(4)).toBe(4);
+    expect(fizzBuzz(17)).toBe(17);
+  });
+
+  test("returns NaN when a string is passed through", () => {
+    expect(fizzBuzz("String")).toBe(NaN);
+  });
 });
