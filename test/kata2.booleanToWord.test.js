93b33a46c631bed450d3f618fe2050868fa2046f
Nina Spray <ninaspray@gmail.com>
2021-01-07 21:11:12 +0000

    Added Test to test

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -4,5 +4,6 @@ describe("booleanToWord", () => {
   it("Returns Yes when passed true, and No when passed false", () => {
     expect(booleanToWord(true)).toBe("Yes");
     expect(booleanToWord(false)).toBe("No");
+    expect(booleanToWord(true)).toBe("Nina");
   });
 });
