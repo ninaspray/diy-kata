40eb6f2be106d6f0c45b35cd8e445086d9e348fe
Nina Spray <ninaspray@gmail.com>
2021-01-09 15:49:50 +0000

    Started Reach Destination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    const Time = (distance / speed).round;
+    return "I should be there in" + Time + "hours.";
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10).toEqual("I should be there in"4.5"hours."));
+    expect(reachDestination(70, 70).toEqual("I should be there in"1.0"hours."));
+    expect(reachDestination(20, 60).toEqual("I should be there in"0.5"hours."));
+  });
 });
