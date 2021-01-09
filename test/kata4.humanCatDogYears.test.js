6182fd23d45beb51a894c80a0805c1ed2beeb9f8
Nina Spray <ninaspray@gmail.com>
2021-01-09 12:36:31 +0000

    Corrected HCDY Test

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -2,8 +2,8 @@ const { humanCatDogYears } = require("../src");
 
 describe("humanCatDogYears", () => {
     xit("returns array of human, cat and dog years when passed human years", () => {
-        expect(10).toBe([10, 56, 64]);
-        expect(20).toBe([20, 96, 114]);
-        expect(33).toBe([33, 148, 179]);
+        expect(humanCatDogYears(10)).toBe([10, 56, 64]);
+        expect(humanCatDogYears(20)).toBe([20, 96, 114]);
+        expect(humanCatDogYears(33)).toBe([33, 148, 179]);
     });
 });
