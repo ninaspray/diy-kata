e79708be7b93638ece6188f6bd47a6fd359cee64
Nina Spray <ninaspray@gmail.com>
2021-01-30 18:03:15 +0000

    reachDestination Refactor

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,11 +1,11 @@
 const reachDestination = (distance, speed) => {
     const Time = Math.round((distance / speed * 2)) / 2.0;
 
-    if (Time < 1) { return "Will be there in less than 1 hour" };
-    return "I should be there in " + Time + " hours.";
+    if (Time <= 0.5) { return "Nearly there" }
+    if (Time >= 0.5 && Time <= 1.0) { return "Will be there in less than 1 hour" }
+    else {return "I should be there in " + Time + " hours."}
 };
 
 
 module.exports = reachDestination;
 
-//if (Time < 0.5) { return "Nearly there" };
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -3,8 +3,8 @@ const { reachDestination } = require("../src");
 describe("reachDestination", () => {
   test("returns string with estimated time of arrival", () => {
     expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours.");
-    expect(reachDestination(70, 70)).toBe("I should be there in 1 hours.");
-    expect(reachDestination(20, 30)).toBe("Will be there in less than 1 hour");
-    expect(reachDestination(20, 50)).toBe("Will be there in less than 1 hour");
+    expect(reachDestination(70, 70)).toBe("Will be there in less than 1 hour");
+    expect(reachDestination(20, 30)).toBe("Nearly there");
+    expect(reachDestination(20, 60)).toBe("Nearly there");
   });
 });
